b21fc1641b56548865b142f32ca28db9eddd414a
zlatinterziev <tendekan.soft@yahoo.com>
2026-05-03 20:38:39 +0300

    SR-0: remoed made with love label.

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -4,8 +4,6 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div>© {new Date().getFullYear()} UnitedSeeds</div>
-      <div className={styles.spacer} />
-      <div className={styles.tag}>Made with ❤ for lifelong learners</div>
     </footer>
   );
 }
